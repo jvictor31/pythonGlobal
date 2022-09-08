2c8dce249d5c40e6d03d4bbe894700d726ca1b1e
João Victor <joaovictorportella@hotmail.com>
2022-09-08 16:12:22 +0000

    adicionados 3 programas em python

---
+++ soma.py
@@ -0,0 +1,5 @@
+print('programa básico de soma. \n')
+num1=int(input('digite o primeiro numero a ser somado :'))
+num2=int(input('digite o segundo numero a ser somado :'))
+resultado=(num1+num2)
+print('soma dos 2 numeros :', resultado)
